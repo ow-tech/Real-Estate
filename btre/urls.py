81cd60ac609c9841d9e35b34a539b476fe402016
ow-tech <wagwanwekon@outlook.com>
2022-08-31 23:48:26 +0400

    ready to deploy

--- btre/urls.py
+++ btre/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL , document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
+
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
