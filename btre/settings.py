abfc337126674fe7f7a130e9cc2c04417fe4ae9a
ow-tech <wagwanwekon@outlook.com>
2022-09-01 13:43:59 +0400

    adding staticfiles_dir

--- btre/settings.py
+++ btre/settings.py
@@ -146,6 +146,9 @@ USE_TZ = True
 # STATICFILES_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 
 
