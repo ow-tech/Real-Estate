3625953150372142c0edd089927d0a3e976d346b
ow-tech <wagwanwekon@outlook.com>
2022-09-02 16:01:41 +0400

    added amenities fields

--- listings/admin.py
+++ listings/admin.py
@@ -5,7 +5,7 @@ from .models import Listing, Floor_Plan, Developer, Area
 class ListingAdmin(admin.ModelAdmin):
     # fields =["realtor","title","developer","area","description", "price","photo_main",("photo_1", "photo_2", "photo_3","photo_4","photo_5", "photo_6", "photo_7","photo_8","photo_9","photo_10","photo_11","photo_12"), "is_published","property_status", "is_handOver_in_12_months", "is_hot","is_villas_and_mansionettes","list_date" ]
     fieldsets = (
-        ('Listing Details', {'fields':("realtor","title","developer","area","description", "price","is_published","property_status", "is_handOver_in_12_months", "is_hot","is_villas_and_mansionettes",)}),
+        ('Listing Details', {'fields':("realtor","title","developer","area", "price","description",'amenities',"kids_facilities","sports_facilities","swimming_pool","highlights","furnished","is_published","property_status", "is_handOver_in_12_months", "is_hot","is_villas_and_mansionettes",)}),
         
         ('Listing Main Photo', {'fields':('photo_main',)}),
         ('Listing Other Photos',{'fields':("photo_1", "photo_2", "photo_3","photo_4","photo_5", "photo_6", "photo_7","photo_8","photo_9","photo_10","photo_11","photo_12",)}),
@@ -17,7 +17,7 @@ class ListingAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'title')
     list_filter = ('realtor',)
     list_editable = ('is_published','property_status','is_handOver_in_12_months','is_hot')
-    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+    search_fields = ('title', 'description', 'price')
     list_per_page = 25
     
 class Floor_PlanAdmin(admin.ModelAdmin):
